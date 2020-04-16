26193da400424ce7480cef60edec62c48a005d1a
IreneChia <850862699@qq.com>
2020-04-16 16:51:24 +0800

    计分器布局

--- app/src/main/java/com/example/firstapp/ScoreActivity.java
+++ app/src/main/java/com/example/firstapp/ScoreActivity.java
@@ -1,11 +1,11 @@
 package com.example.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ScoreActivity extends AppCompatActivity {
 
